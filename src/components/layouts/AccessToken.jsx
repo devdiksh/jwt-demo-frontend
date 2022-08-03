7ce4e6287bdfc8cf1cfd022b4f091bd49d373945
devdiksh <deekshant.dev@gmail.com>
2022-08-04 03:55:02 +0530

    Logout

--- src/components/layouts/AccessToken.jsx
+++ src/components/layouts/AccessToken.jsx
@@ -1,29 +1,29 @@
 import React from "react";
+import axiosClient from "../../axiosClient";
 
 const AccessToken = () => {
-  const handleLogout = () => {
-    window.localStorage.removeItem("accessToken");
-    window.location.reload()
+  const handleLogout = async () => {
+    try {
+      await axiosClient.postRequest("/logout");
+      window.localStorage.removeItem("accessToken");
+    } catch (err) {
+      console.error("Logout Failed", err);
+    }
+    window.location.reload();
   };
 
-  const accessToken = window.localStorage.getItem("accessToken")
+  const accessToken = window.localStorage.getItem("accessToken");
 
-  const header = accessToken.split('.')[0]
-  const payload = accessToken.split('.')[1]
-  const signature = accessToken.split('.')[2]
+  const header = accessToken.split(".")[0];
+  const payload = accessToken.split(".")[1];
+  const signature = accessToken.split(".")[2];
   return (
     <div>
       Access Token
       <div className="Jwtbox">
-        <span className="Jwt-text-red">
-         {`${header}.`}
-        </span>
-        <span className="Jwt-text-violet">
-        {`${payload}.`}
-        </span>
-        <span className="Jwt-text-blue">
-        {`${signature}`}
-        </span>
+        <span className="Jwt-text-red">{`${header}.`}</span>
+        <span className="Jwt-text-violet">{`${payload}.`}</span>
+        <span className="Jwt-text-blue">{`${signature}`}</span>
       </div>
       <button className="Submit" onClick={handleLogout}>
         Logout
